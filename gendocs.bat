a94eb5b1e07d393bf4028e82952e32d26beeef4c
Nathan Woltman <nwoltman@outlook.com>
2015-01-01 02:00:56 -0500

    Improve the gendocs script by starting a local server and opening the docs after they have been generated

--- gendocs.bat
+++ gendocs.bat
@@ -1,5 +1,6 @@
 CALL tools\jsdoc-runner.bat
 php tools\srcdir-fixer.php "%CD%/src/"
 java -jar tools\htmlcompressor.jar --type html -o ./dist/ ./dist/
-.\dist\index.html
-exit
+cd dist
+START "Local PHP Server" /MIN php -S localhost:8000
+START http://localhost:8000
--- gendocs.sh
+++ gendocs.sh
@@ -3,7 +3,9 @@
 ./tools/jsdoc-runner.sh
 php ./tools/srcdir-fixer.php "$PWD/src/"
 java -jar ./tools/htmlcompressor.jar --type html -o ./dist/ ./dist/
-firefox ./dist/index.html &
-# wait for Firefox to load
-sleep 4
-exit
+cd dist
+php -S localhost:8000 &
+PID=$!
+echo PHP server running with PID: $PID
+x-www-browser http://localhost:8000
+kill $PID
